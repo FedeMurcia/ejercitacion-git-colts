01cc2b1f7e530ac8bc1d5e9ccd5ce614709d0ef7
oliva.miguelh@gmail.com <oliva.miguelh@gmail.com@73a22201-0fc0-0ccf-f727-b383aaa4aa3b>
2012-07-05 03:48:15 +0000

    Agregado el toString que necesitaba. :P

--- trunk/tp-final/src/main/java/org/sg1/tpfinal/model/State.java
+++ trunk/tp-final/src/main/java/org/sg1/tpfinal/model/State.java
@@ -1,11 +1,21 @@
 package org.sg1.tpfinal.model;
 
 public enum State {
-	OPEN, // abierta
-	SCOPED, // en análisis
-	IN_PROGRESS, // en curso
-	RESOLVED, // resuelta
-	REOPENED, // reabierta
-	CLOSED; // cerrada
+	OPEN("OPEN"), // abierta
+	SCOPED("SCOPED"), // en análisis
+	IN_PROGRESS("IN PROGRESS"), // en curso
+	RESOLVED("RESOLVED"), // resuelta
+	REOPENED("REOPENED"), // reabierta
+	CLOSED("CLOSED"); // cerrada
 
+	private final String displayableName;
+
+	private State(final String displayableName) {
+		this.displayableName = displayableName;
+	}
+
+	@Override
+	public String toString() {
+		return displayableName;
+	}
 }
